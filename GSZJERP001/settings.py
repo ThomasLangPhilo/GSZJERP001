21567b4f4a57f51df3b01d0a4d67d0761dedb358
ly看分析哲学呢 <12321049+lykfxzxn@user.noreply.gitee.com>
2023-01-10 22:40:36 +0800

    1

--- GSZJERP001/settings.py
+++ GSZJERP001/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test4',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': 'caonimad1',
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
